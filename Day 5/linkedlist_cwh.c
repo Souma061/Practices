b368141bf7bbf048a8c0e2beb46bf9eb524b009e
Souma061 <soumabrataghosh57@gmail.com>
2025-07-15 19:39:22 +0530

    new linkedlist_cwh

---
+++ Day 5/linkedlist_cwh.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+  int data;
+  struct Node *next;
+};
+
+int main() {
+  struct Node *head = (struct Node *)malloc(sizeof(struct Node));
+  struct Node *second = (struct Node *)malloc(sizeof(struct Node));
+  struct Node *third = (struct Node *)malloc(sizeof(struct Node));
+
+  head->data = 5;
+  head->next = second;
+
+  second->data = 10;
+  second->next = third;
+
+  third->data = 15;
+  third->next = NULL;
+
+  
+
+}
