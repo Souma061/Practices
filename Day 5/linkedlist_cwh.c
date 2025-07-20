c312ea9224d8491af8da9e196113c653197bd216
Souma061 <soumabrataghosh57@gmail.com>
2025-07-20 09:08:56 +0530

    newer

--- Day 5/linkedlist_cwh.c
+++ Day 5/linkedlist_cwh.c
@@ -10,6 +10,7 @@ int main() {
   struct Node *head = (struct Node *)malloc(sizeof(struct Node));
   struct Node *second = (struct Node *)malloc(sizeof(struct Node));
   struct Node *third = (struct Node *)malloc(sizeof(struct Node));
+  struct Node *fourth = (struct Node *)malloc(sizeof(struct Node));
 
   head->data = 5;
   head->next = second;
@@ -18,8 +19,21 @@ int main() {
   second->next = third;
 
   third->data = 15;
-  third->next = NULL;
+  third->next = fourth;
 
-  
+  fourth->data = 1223;
+  fourth->next = NULL;
+
+  printf("Linked List: \n");
+  struct Node *current = head;
+  while (current != NULL) {
+    printf("%d -> ", current->data);
+    current = current->next;
+  }
+  printf("NULL\n");
+
+  return 0;
+
+
 
 }
