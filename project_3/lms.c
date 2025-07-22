c81c53b1f839a318b4534ebd4f3d7671f57ba41c
SOUMABRATA GHOSH <Soumabrataghosh57@gmail.com>
2025-07-22 03:25:14 +0000

    added search by ID and Delete book functionality

--- project_3/lms.c
+++ project_3/lms.c
@@ -15,7 +15,7 @@ void addBook() {
   FILE *fp;
   struct library book;
 
-  fp = fopen("library.txt" , "a");
+  fp = fopen("library.txt" , "ab");
   if(fp == NULL) {
     printf("Error opening file;\n");
     return;
@@ -43,9 +43,113 @@ void addBook() {
   printf("\n✅ Book added successfully!\n");
 }
 
+void displayBooks() {
+  FILE *fp;
+  struct library book;
+  int found = 0;
+
+  fp = fopen("library.txt" , "rb");
+  if(fp == NULL) {
+    printf("Error opening file.\n");
+    return;
+  }
+
+  printf("\n📚 List of Books:\n");
+  while(fread(&book, sizeof(book) , 1 , fp)) {
+    found = 1;
+    printf("-------------------------\n");
+    printf("ID: %d\n" , book.ID);
+    printf("Book Name: %s\n" , book.bookName);
+    printf("Author: %s\n" , book.author);
+    printf("Quantity: %d\n" , book.quantity);
+    printf("-------------------------\n");
+  }
+  if(!found) {
+    printf("📭 No books found in the library.\n");
+  }
+  fclose(fp);
+
+}
+
+void searchBook() {
+  FILE *fp;
+  struct library book;
+  int found = 0, searchID;
+
+  fp = fopen("library.txt" , "rb");
+  if(fp == NULL) {
+    printf("Error opening file.\n");
+    return;
+  }
+   printf("Enter Book ID to search: ");
+   scanf("%d" , &searchID);
+
+   while(fread(&book, sizeof(book) , 1 , fp)) {
+    if(book.ID == searchID) {
+      found = 1;
+      printf("\n📖 Book Found:\n");
+      printf("-------------------------\n");
+      printf("ID: %d\n" , book.ID);
+      printf("Book Name: %s\n" , book.bookName);
+      printf("Author: %s\n" , book.author);
+      printf("Quantity: %d\n" , book.quantity);
+      printf("-------------------------\n");
+      break;
+    }
+   }
+   if(!found) {
+    printf("❌ Book with ID %d not found.\n", searchID);
+   }
+   fclose(fp);
+}
+
+void deleteBook() {
+  FILE *fp, *temp;
+  struct library book;
+  int found = 0, deleteID;
+
+  fp = fopen("library.txt" , "rb");
+  temp = fopen("temp.txt" , "wb");
+
+  if(fp == NULL || temp == NULL) {
+    printf("Error opening file.\n");
+    return;
+  }
+
+  printf("Enter Book ID to delete: ");
+  scanf("%d" , &deleteID);
+
+  while(fread(&book, sizeof(book) , 1 , fp)) {
+    if(book.ID == deleteID) {
+      found = 1;
+      printf("Book with ID %d deleted successfully.\n", deleteID);
+      continue;;
+    }
+    fwrite(&book , sizeof(book) , 1 , temp);
+  }
+  fclose(fp);
+  fclose(temp);
+
+  remove("library.txt");
+  rename("temp.txt" , "library.txt");
+
+  if(!found) {
+    printf("❌ Book with ID %d not found.\n", deleteID);
+  } else {
+    printf("✅ Book deleted successfully!\n");
+  }
+}
+
+void updateBook() {
+  
+}
+
 
 int main() {
   addBook();
+  displayBooks();
+  searchBook();
+  deleteBook();
   return 0;
 }
 
